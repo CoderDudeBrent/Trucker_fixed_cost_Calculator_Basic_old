a09a46e536e105b22c2852be54abb0c1a2265ca0
CoderDudeBrent <brent.labs1@gmail.com>
2022-12-15 23:34:32 -0600

    Initial commit failed Retrying

---
+++ main.py
@@ -0,0 +1,45 @@
+#Fixed costs
+
+print("Fixed cost information:")
+
+while True:
+    try:
+        truck_payment = float(input('What is your monthly truck payment: '))
+        break
+    except ValueError:
+            print("Please enter a number here...")
+
+while True:
+    try:
+        insurance = float(input('What are your total insurance payment amounts (Monthly): '))
+        break
+    except ValueError:
+        print('Same as last time, you have to type a number here...')
+
+
+#Insurance and stuff
+
+#License plates
+
+#Permits
+
+#Total these numbers
+
+# ________ Variable costs
+
+#fuel
+
+#Food
+
+#Frieght related costs
+
+#Truck maint
+
+#miscellanious expenses
+
+# ____________Total miles driven__________
+
+#_____________ Fixed cost / total miles driven __________ Fixed cost per mile
+
+#_____________ Variable cost / total miles driven __________ Variable cost per mile
+
---
+++ truckerfixedcost/fixedcost/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.fixedcost, name='fixedcost'),
+    path('calculate_cost/', views.calculate_cost, name='calculate_cost', ),
+
+]
